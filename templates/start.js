77b2a5ad84abcef3d27004a69db6e66ac0a222b5
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2024-03-16 19:04:24 +0100

    template start HTML-Gerüst

---
+++ templates/start.js
@@ -0,0 +1,21 @@
+
+/** JSDoc Die Funktion generiert die Buttons für den Bereich der Startseite mit allen Funktionen */
+function renderStartPage(){
+return /*html*/ `
+<div class="startcontainer">
+    <div><h1>JOIN</h1><div class="border-left cyan">Key Metrics at a Glance</div></div
+    <div>
+        <div id="start-todo"></div>
+        <div id="start-done"></div>
+        <div id="start-urgent" colspan="2"></div>
+        <div id="start-tasks"></div>    
+        <div id="start-greeting" rowspan="3"><div>
+    </div>
+
+
+
+</div>
+
+`
+
+}
