e63ff22b8e58cf96e656e5476127dac818a02b73
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2024-03-21 22:48:50 +0100

    animation still not working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // TOKEN FÜR SPÄTER REMOTE DATENSPEICHERUNG  VJOV8Y8IUZO3FYNKTN886RO1F4GPQUZG86SP8SQP
+
+function init(){
+    introAnimation();
+}
+
+function introAnimation(){
+    document.getElementById("logo").classList.add("animation");
+    document.getElementById("content").classList.add("visible");
+}
