7d3b07f1dc52ed866ab906776d38b289b1b95599
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2024-03-25 19:18:18 +0100

    frame-pages - summary included

--- js/loadpages.js
+++ js/loadpages.js
@@ -1,5 +1,5 @@
-/** JSDOC 
- * Hiermit wird die Start-Seite mit der Übersicht aller Tasks erstellt
+/**
+ * Declaring all sections that wil be filled up with content per Click on NavBar-Menu-point
  */
 
 const summary = document.getElementById("summary");
@@ -8,9 +8,9 @@ const board = document.getElementById("board");
 const contacts = document.getElementById("contacts");
 const policy = document.getElementById("policy");
 
-
-
-
+/**
+ * CASE: Loading Summary
+ */
 
 function loadSummary() {
     summary.classList.remove("displaynone");
@@ -22,7 +22,10 @@ policy.classList.add("displaynone");
     summary.innerHTML = renderSummary();
     renderSayHello();
 }
-
+/** JSDoc 
+ * Die Funktion generiert die Buttons für den Bereich der Summary-Content mit allen Informationen
+ * @param ${Name}: this is the User, who is logged in.
+ */
 function renderSayHello() {
     let greetings = sayHello();
     let place = document.getElementById("summary-greet");
@@ -31,6 +34,9 @@ function renderSayHello() {
                     `;
 }
 
+/**
+ * UHRZEIT-Abhängige Begrüßung
+ */
 function sayHello() {
     const jetzt = new Date();
     const stunde = jetzt.getHours();
@@ -44,49 +50,4 @@ function sayHello() {
     } else {
         return "Gute Nacht,";
     }
-}
-
-function renderSummary() {
-    return /*html*/ `
-    
-    <div class="start-container">
-        <div class="start-head"><h2 class="padding20 right-border-2px">Join 360</h2><h4 class="padding20">Key Metrics at a Glance</h4>
-    </div>
-    <div class ="summarycontainer">
-       
-            <div id="summary-todo" class="tile flex-center border-bg">
-                <div id="todo" class="circle bg-dark edit"></div>
-                <div class="flex-center flex-column"><h2>1</h2><br>
-                <h4>To-do</h4></div>
-            </div>
-            <div id="summary-done" class="tile flex-center border-bg">
-                <div id="done" class="circle bg-dark check"></div>
-                <div class="flex-center flex-column"><h2>1</h2><br>
-                <h4>Done</h4></div>
-            </div>
-            <div id="summary-urgent" class="tile flex-center border-bg">
-                <div id="urgent" class="circle bg-urgent urgent">
-                </div>
-                 <div class="flex-center flex-column">
-                        <h2>1</h2><br>
-                    <span class="span">Urgent</span>
-                </div>
-            
-                <div class="left-border-2px">
-                    <span class="bold span">16. Oktober 2024</span><br>
-                    <span class="span">Upcoming Deadline</span>
-                </div>
-            </div>
-            <div id="summary-board" class="tile flex-center border-bg">
-                <div class="flex-center flex-column"><h2>1</h2><br>
-                    <h4>Tasks in <br>Board</h4></div>
-                </div>
-            <div id="summary-progress" class="tile flex-center border-bg"><div class="flex-center flex-column"><h2>2</h2><br>
-                <h4>Tasks in <br>Progress</h4></div></div>   
-            <div id="summary-feedback" class="tile flex-center border-bg"><div class="flex-center flex-column"><h2>2</h2><br>
-                <h4>Awaiting <br>Feedback</h4></div></div>    
-            <div id="summary-greet">7</div>
-    </div>
-
-    `
 }
---
+++ templates/summary.js
@@ -0,0 +1,43 @@
+function renderSummary() {
+return /*html*/ `
+    <div class="start-container">
+        <div class="start-head"><h2 class="padding20 right-border-2px">Join 360</h2><h4 class="padding20">Key Metrics at a Glance</h4>
+    </div>
+    <div class ="summarycontainer">
+       
+            <div id="summary-todo" class="tile flex-center border-bg">
+                <div id="todo" class="circle bg-dark edit"></div>
+                <div class="flex-center flex-column"><h2>1</h2><br>
+                <h4>To-do</h4></div>
+            </div>
+            <div id="summary-done" class="tile flex-center border-bg">
+                <div id="done" class="circle bg-dark check"></div>
+                <div class="flex-center flex-column"><h2>1</h2><br>
+                <h4>Done</h4></div>
+            </div>
+            <div id="summary-urgent" class="tile flex-center border-bg">
+                <div id="urgent" class="circle bg-urgent urgent">
+                </div>
+                 <div class="flex-center flex-column">
+                        <h2>1</h2><br>
+                    <span class="span">Urgent</span>
+                </div>
+            
+                <div class="left-border-2px">
+                    <span class="bold span">16. Oktober 2024</span><br>
+                    <span class="span">Upcoming Deadline</span>
+                </div>
+            </div>
+            <div id="summary-board" class="tile flex-center border-bg">
+                <div class="flex-center flex-column"><h2>1</h2><br>
+                    <h4>Tasks in <br>Board</h4></div>
+                </div>
+            <div id="summary-progress" class="tile flex-center border-bg"><div class="flex-center flex-column"><h2>2</h2><br>
+                <h4>Tasks in <br>Progress</h4></div></div>   
+            <div id="summary-feedback" class="tile flex-center border-bg"><div class="flex-center flex-column"><h2>2</h2><br>
+                <h4>Awaiting <br>Feedback</h4></div></div>    
+            <div id="summary-greet">7</div>
+    </div>
+
+    `;
+}
