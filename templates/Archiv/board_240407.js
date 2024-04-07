11da9c56eea7d61b14a2a62eae67e5ca711e7c63
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2024-04-07 22:42:06 +0200

    Nicht benutzte Templates in Archiv-Ordner gepackt

--- templates/Archiv/board_240407.js
+++ templates/Archiv/board_240407.js
@@ -4,7 +4,7 @@ let todos = [{
     "title": "[0] Putzen",
     "description": "nicht nur sauber, sondern rein",
     "priority": "urgent",
-    "workers": [],
+    "workers": [4],
     "type": "User Story"
 },
 {
@@ -13,26 +13,26 @@ let todos = [{
     "category": "inProgress",
     "description": "auch wenn sie nicht wollen",
     "priority": "medium",
-    "workers": [1, 2, 3, 4],
+    "workers": [1, 2, 3,4],
     "type": "User Story"
-},
+   },
 {
     "id": 2,
     "title": "[2] Schokolade aufessen",
     "category": "inDone",
     "description": "... super task",
     "priority": "urgent",
-    "workers": [1, 2, 3, 4],
+    "workers": [1, 2, 3,4],
     "type": "Technical Task"
 },
 {
-    "id": 3,
+   "id": 3,
     "title": "[3] Hirn benutzen",
     "category": "inProgress",
     "description": "morgen vielleicht",
     "priority": "medium",
     "workers": [1, 2, 3],
-    "type": "Technical Task"
+   "type": "Technical Task"
 },
 {
     "id": 4,
@@ -52,7 +52,7 @@ let todos = [{
     "priority": "low",
     "workers": [1, 2, 3],
     "type": "Technical Task"
-},
+    },
 {
     "id": 6,
     "title": "[6] Schäfchen zählen",
@@ -64,7 +64,7 @@ let todos = [{
     "type": "User Story"
 },
 {
-    "id": 7,
+    "id":7,
     "title": "[7] Schäfchen zählen",
     "category": "inDone",
     "description":
@@ -74,13 +74,13 @@ let todos = [{
     "type": "Technical Task"
 },
 {
-    "id": 8,
+    "id":8,
     "title": "[8] Schäfchen zählen",
     "category": "awaitFeedback",
     "description":
         "Wenn Schäfchen Schäfchen zählen, zählen sie sich dann mit? ",
     "priority": "low",
-    "workers": [1, 2, 3, 4],
+    "workers": [1, 2, 3,4],
     "type": "Technical Task"
 },
 {
@@ -90,7 +90,7 @@ let todos = [{
     "description":
         "Wenn Schäfchen Schäfchen zählen, zählen sie sich dann mit? ",
     "priority": "low",
-    "workers": [1, 2, 3, 4],
+    "workers": [1, 2, 3,4],
     "type": "User Story"
 }];
 
@@ -101,10 +101,10 @@ let currentTasks;
 
 
 async function loadAllTasks() {
-    let userResponse = await fetch(loadUsers).catch(errorFunction);
-    //let taskResponse = await fetch(todos).catch(errorFunction);
-    currentUser = await userResponse.json();
-    //currentTasks = await taskResponse.json();
+   let userResponse = await fetch(loadUsers).catch(errorFunction);
+   //let taskResponse = await fetch(todos).catch(errorFunction);
+   currentUser = await userResponse.json();
+   //currentTasks = await taskResponse.json();
 
     console.log("loaded User:", currentUser);
     console.log("loaded Tasks:", currentTasks);
@@ -113,13 +113,13 @@ async function loadAllTasks() {
 
 function errorFunction() {
     console.log("Fehler aufgetreten");
-}
+  }
 
 let currentDraggedElement;
 
 async function updateHTML() {
     await loadAllTasks();
-    //  await loadTodoLane();
+  //  await loadTodoLane();
     let toDo = todos.filter(t => t['category'] == 'inTodo');
     let inProgress = todos.filter(t => t['category'] == 'inProgress');
     let awaitFeedback = todos.filter(t => t['category'] == 'awaitFeedback');
@@ -128,48 +128,33 @@ async function updateHTML() {
     let laneTodo = document.getElementById('inTodo');
     laneTodo.innerHTML = '';
     for (let index = 0; index < toDo.length; index++) {
-        if (toDo.length !== 0) {
-            const element = toDo[index];
-            laneTodo.innerHTML += await renderTaskHTML(element);
-            await backgroundType(element);
-        } else {
-            laneTodo.innerHTML = renderEmptyLane();
-        }
+        const element = toDo[index];
+        laneTodo.innerHTML += generateTodoHTML(element);
     }
 
     let laneProgress = document.getElementById('inProgress');
     laneProgress.innerHTML = '';
     for (let index = 0; index < inProgress.length; index++) {
         const element = inProgress[index];
-        laneProgress.innerHTML += await renderTaskHTML(element);
-        await backgroundType(element);
+        laneProgress.innerHTML += generateTodoHTML(element);
     }
 
     let laneAwait = document.getElementById('awaitFeedback');
     laneAwait.innerHTML = '';
     for (let index = 0; index < awaitFeedback.length; index++) {
         const element = awaitFeedback[index];
-        laneAwait.innerHTML += await renderTaskHTML(element);
-        await backgroundType(element);
+        laneAwait.innerHTML += generateTodoHTML(element);
     }
 
     let laneDone = document.getElementById('inDone');
     laneDone.innerHTML = '';
     for (let index = 0; index < done.length; index++) {
         const element = done[index];
-        laneDone.innerHTML += await renderTaskHTML(element);
-        await backgroundType(element);
+        laneDone.innerHTML += generateTodoHTML(element);
     }
 }
 
-function renderEmptyLane() {
-    return /*html*/ `
-    <div class="emptylane"> No tasks yet.</div>
-  `
-}
-
-
-/* async function loadTodoLane(){
+async function loadTodoLane(){
  let todoTasks = currentUser[1].items.todos;
  let laneTodo = document.getElementById('inTodo');
 for(let i = 0; i < todoTasks.length; i++){
@@ -186,7 +171,7 @@ function fillCard(id){
      laneTodo.innerHTML += generateTodoHTML(element);
  }
 
-}*/
+}
 
 function startDragging(id) {
     currentDraggedElement = id;
@@ -194,32 +179,6 @@ function startDragging(id) {
 
 function generateTodoHTML(element) {
     return `<div draggable="true" ondragstart="startDragging(${element['id']})" class="task">${element['title']} </div>`;
-}
-
-async function renderTaskHTML(element) {
-
-    return /*html*/ `
-        <div draggable="true" ondragstart="startDragging(${element['id']})" class="task">
-            <div id='type${element['id']}' class="task-type">${element['type']}</div>
-                <div class="task-text">
-                    <div class="task-title bold"> ${element['title']} </div>
-                    <div class="task-description text-greyish">${element['description']}</div>
-                </div>
-            <div class="task-footer">
-                <div class="task-worker"></div>
-                <div class="task-prio"><img src="../assets/svg/${element['priority']}.svg" class="prioSVG"></div>
-            </div>
-        </div>
-    `;
-}
-
-async function backgroundType(element) {
-
-    if (element['type'] == "Technical Task") {
-        document.getElementById(`type${element['id']}`).classList.add("bg-technicaltask");
-    } else {
-        document.getElementById(`type${element['id']}`).classList.add("bg-userstory")
-    };
 }
 
 function allowDrop(ev) {
