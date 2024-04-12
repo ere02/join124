346670a34fb27b58f3e42b7321254c66568be5b4
C. Troitzsch <150924499+CT-SPACE@users.noreply.github.com>
2024-04-12 18:53:25 +0200

    ContactList enviroment

--- users_storage.js
+++ users_storage.js
@@ -36,6 +36,7 @@ const allUsers =
       "pword": "*****",
       "email": "ere02@bavarian.de",
       "projectId":[1],
+      "contactId":[1,3,4]
       // "items":{
       //   "todos":[1],
       //   "progress":[9,3,5],
@@ -51,6 +52,7 @@ const allUsers =
         "pword": "*****",
         "email": "ardit@Qoku.de",
         "projectId":[1],
+        "contactId":[1,2,4,6,7,8,9,10]
         // "items":{
         //   "todos":[1],
         //   "progress":[9,3,5],
@@ -72,5 +74,65 @@ const allUsers =
         //   "await":[8],
         //   "done":[2]   
         // }
-    }
+      },
+      {
+        "id":5,
+        "nickname": "testuser1",
+        "firstname":"T.",
+        "familyname": "EstUser",
+        "pword": "*****",
+        "email": "t.estuser@web.de",
+        "projectId":[1],
+        "contactId":[1,2,4]
+      },
+      {
+        "id":6,
+        "nickname": "testuser2",
+        "firstname":"Test",
+        "familyname": "User",
+        "pword": "*****",
+        "email": "test@user.de",
+        "projectId":[1],
+        "contactId":[1,2,4]
+      },
+      {
+        "id":7,
+        "nickname": "testuser3",
+        "firstname":"User",
+        "familyname": "Test",
+        "pword": "*****",
+        "email": "user.test@gmx.de",
+        "projectId":[1],
+        "contactId":[1,2,4]
+      },
+      {
+        "id":8,
+        "nickname": "testuser4",
+        "firstname":"U.",
+        "familyname": "Test",
+        "pword": "*****",
+        "email": "u.test@yahoo.de",
+        "projectId":[1],
+        "contactId":[1,2,4]
+      },
+      {
+        "id":9,
+        "nickname": "testuser5",
+        "firstname":"Tess",
+        "familyname": "Tuser",
+        "pword": "*****",
+        "email": "tess.tuser@gmail.com",
+        "projectId":[1],
+        "contactId":[1,2,4]
+      },
+      {
+        "id":10,
+        "nickname": "testuser6",
+        "firstname":"Testu",
+        "familyname": "Ser",
+        "pword": "*****",
+        "email": "test34@googlemail.de",
+        "projectId":[1],
+        "contactId":[1,2,4]
+      }
 ]
