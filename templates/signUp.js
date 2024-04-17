9aaaf4c83c929a70d3c94e5098e0e48ca0a26e25
ere02 <tiltmaster2@googlemail.com>
2024-04-17 18:33:52 +0200

    testting update on signup.htlm

--- templates/signUp.js
+++ templates/signUp.js
@@ -1,78 +1,57 @@
 function signUp() {
+    // Get the element with class "new-user"
+    const newUserDiv = document.querySelector('.new-user');
+
+    // Hide the element using style.display
+    newUserDiv.style.display = 'none';
+  
   let content = document.getElementById('content');
-  content.innerHTML =`
-
-      <div class="signup-wrapper">
-        <div class="input-container-signup">
-          <div class="headline-wrapper-signup">
-
-          <img
-          src="/assets/svg/arrow-left-line.svg"
-          <div class="go-back-arrow" onclick="goMainPage()">
-              alt=""
-              class="arrow-icon"
-          /*     onclick="window.location.href='index.html'" */
-            />
-            <div>
-              <p class="headline">Sign up</p>
-              <hr class="underline" />
-            </div>
-          </div>
-          <div class="input-wrapper">
-            <input
-              id="name"
-              class="input-field"
-              type="text"
-              name="name"
-              placeholder="Name"
-              required
-            />
-            <img src="/assets/svg/person.svg" alt="lock icon" class="icon" />
-          </div>
-          <div class="input-wrapper">
-            <input
-              id="email"
-              class="input-field"
-              type="text"
-              name="email"
-              placeholder="email"
-              required
-            />
-            <img src="/assets/svg/mail.svg" alt="email icon" class="icon" />
-          </div>
-          <div class="input-wrapper">
-            <input
-              id="password"
-              class="input-field"
-              type="text"
-              name="password"
-              placeholder="Password"
-              required
-            />
-            <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
-          </div>
-          <div class="input-wrapper">
-            <input
-              id="confirmPassword"
-              class="input-field"
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirm Password"
-              required
-            />
-            <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
-          </div>
-          <div class="checkbox-container">
-            <input type="checkbox" name="police" id="police" />
-            <label for="remember"
-              >I accept the <a href="#">Privacy policy</a></label
-            >
-          </div>
-          <div class="submit-container" id="submit">
-            <button class="button" type="submitLogin">Sign up</button>
+  content.innerHTML = `
+  <div class="input-container-signUp">
+    <div class="signup-wrapper">
+      <div class="input-container-signup">
+        <div class="headline-wrapper-signup">
+          <a href="index.html">
+            <img src="/assets/svg/arrow-left-line.svg" alt="" class="arrow-icon" />
+          </a>
+          <div>
+            <p class="headline">Sign up</p>
+            <hr class="underline" />
           </div>
         </div>
+        <div class="input-wrapper">
+          <input id="name" class="input-field" type="text" name="name" placeholder="Name" required />
+          <img src="/assets/svg/person.svg" alt="lock icon" class="icon" />
+        </div>
+        <div class="input-wrapper">
+          <input id="email" class="input-field" type="text" name="email" placeholder="email" required />
+          <img src="/assets/svg/mail.svg" alt="email icon" class="icon" />
+        </div>
+        <div class="input-wrapper">
+          <input id="password" class="input-field" type="text" name="password" placeholder="Password" required />
+          <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
+        </div>
+        <div class="input-wrapper">
+          <input id="confirmPassword" class="input-field" type="password" name="confirmPassword"
+            placeholder="Confirm Password" required />
+          <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
+        </div>
+        <div class="checkbox-container">
+          <input type="checkbox" name="police" id="police" />
+          <label for="remember">I accept the <a href="#">Privacy policy</a></label>
+        </div>
+        <div class="submit-container" id="submit">
+          <button class="button" type="submitLogin">Sign up</button>
+        </div>
       </div>
-
+    </div>
+  </div>
   `;
+
+  const goBack = () => {
+    window.location.href = 'index.html';
+  };
+
+  const arrowIcon = document.querySelector('.arrow-icon');
+  arrowIcon.addEventListener('click', goBack);
 }
