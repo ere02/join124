73baa442ad9d75cb02548a17c6a7c6642bf26f3b
ere02 <tiltmaster2@googlemail.com>
2024-04-29 23:18:55 +0200

    new register form

--- templates/signUp.js
+++ templates/signUp.js
@@ -7,46 +7,24 @@ function signUp() {
 
   let content = document.getElementById('content');
   content.innerHTML = /*html*/ `
-  <div class="input-container-signUp">
-  <div class="signup-wrapper">
-    <div class="input-container-signup">
-      <div class="headline-wrapper-signup">
-        <div onclick="renderLogin()">
-          <img src="/assets/svg/arrow-left-line.svg" alt="" class="arrow-icon" />
-        </div>
-        <div class="headline-wrapper">
-          <p class="headline">Sign up</p>
-          <hr class="underline" />
-        </div>
+    <form onsubmit="register()">
+      <div>
+        <input type="text" id="name-reg" placeholder="Name" >
       </div>
-      <div class="input-wrapper">
-        <form onsubmit="register(); return false;">
-          <input id="name" class="input-field" type="text" name="name" placeholder="Name" required />
-          <img src="/assets/svg/person.svg" alt="lock icon" class="icon" />
+      <div>
+        <input type="email" id="email-reg" placeholder="Email" >
       </div>
-      <div class="input-wrapper">
-        <input id="email" class="input-field" type="email" name="email" placeholder="email" required />
-        <img src="/assets/svg/mail.svg" alt="email icon" class="icon" />
+      <div>
+        <input type="password" id="password-reg" placeholder="Password" >
       </div>
-      <div class="input-wrapper">
-        <input id="password" class="input-field" type="password" name="password" placeholder="Password" required />
-        <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
+      <div>
+        <input type="password" id="confirmPassword-reg" placeholder="Confirm Password" >
       </div>
-      <div class="input-wrapper">
-        <input id="confirmPassword" class="input-field" type="password" name="confirmPassword"
-          placeholder="Confirm Password" required />
-        <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
+      <div>
+        <input type="checkbox" id="agree-reg">
+        <label for="agree-reg">I agree to the terms and conditions</label>
       </div>
-      <div class="checkbox-container">
-        <input type="checkbox" name="police" id="police" />
-        <label for="remember">I accept the <a href="#">Privacy policy</a></label>
-      </div>
-      <div class="submit-container">
-        <button id="registerBtn" class="button" type="submit">Sign up</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
+      <button type="submit">Register</button>
+    </form>
   `;
 }
