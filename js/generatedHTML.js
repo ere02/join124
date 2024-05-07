4f1baa11b533577092f640cbe3492c314ef1231c
ere02 <tiltmaster2@googlemail.com>
2024-05-07 18:26:09 +0200

    register & storage working?!

--- js/generatedHTML.js
+++ js/generatedHTML.js
@@ -28,3 +28,36 @@ function generateSignUpHTML() {
    `;
  }
 //LogIn
+
+function loginPage() {
+  return `
+
+      <div class="input-container-login">
+        <div class="headline-wrapper">
+          <p class="headline">Log in</p>
+          <hr class="underline" />
+        </div>
+        <div class="input-wrapper">
+          <input class="input-field" type="text" name="email" id="email" placeholder="E-mail" required />
+          <img src="/assets/svg/mail.svg" alt="email icon" class="icon" />
+        </div>
+        <div class="input-wrapper">
+          <input class="input-field" type="password" name="password" id="password" placeholder="Password" required />
+          <img src="/assets/svg/lock.svg" alt="lock icon" class="icon" />
+        </div>
+        <div class="checkbox-container">
+          <input type="checkbox" name="remember" id="remember" />
+          <label for="remember">Remember me</label>
+        </div>
+        <div class="submit-container" id="submit">
+          <button class="button" type="submit"><h5>Log in</h5></button>
+          <button class="button-guest" type="submit"><h5>Guest Log in</h5></button>
+        </div>
+      </div>
+      <div class="login-footer">
+        <span class="legal-notice-link" onclick="callFramePageSinLogin('privacy')">
+          Privacy Policy</span><span class="legal-notice-link" onclick="callFramePageSinLogin('legal')">Legal notice</span>
+      </div>
+    </div>
+  `;
+}
